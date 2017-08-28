719458417dd60a41692362ab49bb84b24cb7fa75
J. Fulmer <jeff@joedog.org>
2017-08-28 04:15:18 -0400

    Collision detection work

--- src/org/joedog/trump/control/Game.java
+++ src/org/joedog/trump/control/Game.java
@@ -1,6 +1,7 @@
 package org.joedog.trump.control;
 
 import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 import org.joedog.util.*;
@@ -9,10 +10,11 @@ import org.joedog.trump.model.*;
 import org.joedog.trump.sprite.*;
 
 public class Game extends AbstractController {
-  private Engine engine;
-  private Thread thread;
-  private Arena  arena;
-  private int    sleep = 22;
+  private Engine  engine;
+  private Thread  thread;
+  private Arena   arena;
+  private int     sleep = 22;
+  private boolean pause = false;
 
   public Game() {
     this.arena = Arena.getInstance(); 
@@ -25,6 +27,10 @@ public class Game extends AbstractController {
 
   public void shoot() {
     this.arena.shoot();
+  }
+
+  public void pause() {
+    this.pause = ! this.pause;
   }
 
   public void setEngine(Engine engine) {
@@ -50,6 +56,9 @@ public class Game extends AbstractController {
     }
 
     while (status != Constants.DONE) {
+      do {
+       Sleep.milliseconds(100);
+      } while (pause);
       switch (status) {
         case Constants.INIT:
           this.runModelMethod("newGame");
@@ -59,7 +68,6 @@ public class Game extends AbstractController {
            long delta = System.currentTimeMillis() - prior;
            prior = System.currentTimeMillis();
            this.move(delta);
-           //System.out.println("SLEEP: "+this.sleep);
            Sleep.milliseconds(this.sleep);
            break;
       }
@@ -69,7 +77,7 @@ public class Game extends AbstractController {
   public synchronized void move(long delta) {
     boolean drop = false;
     for (Actor a : (ArrayList<Actor>)this.getModelProperty("Actors")) {
-      if (a instanceof Missile) {
+      if (a instanceof Missile || a instanceof Bomb) {
         detectCollision(a);
       }
     }
@@ -97,18 +105,120 @@ public class Game extends AbstractController {
       }
     }
     for (Actor a : (ArrayList<Actor>)this.getModelProperty("Actors")) {
-       a.move(delta);
+      if (RandomUtils.range(1,100000) <= 50) {
+        Bomb bomb = new Bomb(a.getX()+24, a.getY());
+        bomb.setDepth((int)this.getModelProperty("Height")-(bomb.getHeight()+89));
+        this.arena.add(bomb);
+      }
+      a.move(delta);
     }
   }
 
+/*************************************************************
+  private void detectCollision(Actor actor) {
+    Rectangle bounds = actor.getBounds();
+
+    for (Actor a : (ArrayList<Actor>)this.getModelProperty("Actors")) {
+      if (a == null || actor.equals(a)) continue;
+      if (a.getType() == actor.getType()) continue;
+      //if (bounds.intersects(a.getBounds())) {
+        // we're in the neighborhood
+        HashSet<String> mask1 = actor.getMask();
+        HashSet<String> mask2 = a.getMask();
+        mask1.retainAll(mask2);
+        System.out.println("INSIDE RECT");
+        if (mask1.size() > 0) {
+          //System.out.println("COLLISION: "+mask1.size());
+          System.out.println(actor.toString()+" collided with a "+a.toString());
+          //System.out.println(mask1.toString());
+          actor.collide(a); 
+          a.collide(actor);
+        } else {
+          System.out.println("NO MATCH");
+        }
+      //}
+    }
+  }
+*************************************************************/
   private void detectCollision(Actor actor) {
     Rectangle bounds = actor.getBounds();
     for (Actor a : (ArrayList<Actor>)this.getModelProperty("Actors")) {
-      if (a == null || actor.equals(a)) continue;
+      if (a == null || actor.equals(a)) continue; 
+      if (actor.getType() == Actor.BOMB && a.getType() == Actor.ALIEN) continue;
       if (bounds.intersects(a.getBounds())) {
-        actor.collide(a);
-        a.collide(actor);
+        if (touches(actor, a)) {
+          System.out.println("Actor "+actor.getName()+" collided with "+a.getName()+" at "+actor.getLocation().toString()+" verified by "+a.getLocation().toString());  
+          actor.collide(a);
+          a.collide(actor);
+        }
       }
     }
+  }
+
+  public static boolean touches(Actor s1, Actor s2) {
+    BufferedImage b1 = s1.getImage();
+    BufferedImage b2 = s2.getImage();
+
+    int xshift = s2.getX()-s1.getX();
+    int yshift = s2.getY()-s1.getY();
+
+    //Test if the Sprites overlap at all
+    if((xshift > 0 && xshift > s1.getWidth()) || (xshift < 0 && -xshift > s2.getWidth())) {
+      return false;
+    }
+
+    if((yshift > 0 && yshift > s1.getHeight()) || (yshift < 0 && -yshift > s2.getHeight())) {
+      return false;
+    }
+
+    //if they overlap, find out in which regions they do
+    int leftx, rightx, topy, bottomy;
+    int leftx2, topy2;
+
+    if(xshift >= 0) {
+      leftx = xshift;
+      leftx2 = 0;
+
+      rightx = Math.min(s1.getWidth(), s2.getWidth()+xshift);
+    } else {
+      rightx = Math.min(s1.getWidth(), s2.getWidth()+xshift);
+
+      leftx = 0;
+      leftx2 = -xshift;
+    }
+  
+    if(yshift >= 0) {
+        topy = yshift;
+        topy2 = 0;
+
+        bottomy = Math.min(s1.getHeight(), s2.getHeight()+yshift);
+    } else {
+        bottomy = Math.min(s1.getHeight(), s2.getHeight()+yshift);
+
+        topy = 0;
+        topy2 = -yshift;
+    }
+
+    //then compare the overlapping regions,
+    //if in any spot both pixels are not transparent, return true
+
+    int ys = bottomy-topy;
+    int xs = rightx-leftx;
+
+    for(int x=0; x<xs; x++) {
+        for(int y=0; y<ys; y++) {
+            int pxl1 = b1.getRGB(leftx+x, topy+y); 
+            int pxl2 = b2.getRGB(leftx2+x, topy2+y);
+            int aph1 = (pxl1 >> 24) & 0xff;
+            int aph2 = (pxl2 >> 24) & 0xff;
+
+            if (aph1 != 0 && aph2 != 0) {
+              System.out.printf("NOT TRANSPARENT %d,%d (%d)  %d,%d (%d)\n", leftx+x, topy+y, aph1, leftx2+x, topy2+y, aph2);
+              return true;
+            }
+        }
+    }
+    System.out.println("RETURNING FALSE!");
+    return false;
   }
 }
